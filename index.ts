57212dec20fa0bc96888569fa3be68504b7c0464
DCTECH02 <davidcyrilx2005@gmail.com>
2024-12-15 15:35:00 +0100

    Add files via upload

--- index.ts
+++ index.ts
@@ -2,51 +2,56 @@ import TelegramBot from 'node-telegram-bot-api';
 import { CommandHandler } from './handlers/CommandHandlers';
 import config from './config';
 import dotenv from 'dotenv';
-import express from 'express';
+import express, { Request, Response } from 'express';
 import path from 'path';
 
+// Load environment variables
 dotenv.config();
 
-const bot = new TelegramBot(`${process.env.TOKEN_BOT}`, { polling: true });
+// Initialize Telegram Bot
+const bot = new TelegramBot(process.env.TOKEN_BOT || '', { polling: true });
 const commandHandler = new CommandHandler(bot, config);
 
+// Declare a global handler
 declare global {
-   var handler: CommandHandler;
+  var handler: CommandHandler;
 }
 global.handler = commandHandler;
 
+// Bot initialization
 async function startBot() {
   try {
     await commandHandler.loadCommands();
-    console.log("Bot started successfully!");
+    console.log('Bot started successfully!');
 
     // Handle graceful shutdown
     process.once('SIGINT', () => {
       bot.stopPolling();
       process.exit(0);
     });
+
     process.once('SIGTERM', () => {
       bot.stopPolling();
       process.exit(0);
     });
   } catch (error) {
-    console.error("Error starting bot:", error);
+    console.error('Error starting bot:', error);
   }
 }
 
 // Start the bot
 startBot();
 
-// Set up the Express app
+// Set up Express app
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Serve static files from the "public" directory
-app.use(express.static(path.join(__dirname, 'public')));
+// Serve static files
+app.use(express.static(path.join(__dirname, '../public')));
 
-// Serve gifted.html as the default route
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+// Serve gifted.html
+app.get('/', (req: Request, res: Response) => {
+  res.sendFile(path.join(__dirname, '../public', 'index.html'));
 });
 
 // Start the server
